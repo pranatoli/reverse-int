32d8233df2d9147d99111f5de8514ed409e99ec2
pranatoli <pr.anatoli@gmail.com>
2022-06-27 15:27:04 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let a
+    if (n < 0) {
+        a = n * -1
+    } else a = n
+    let str = String(a)
+    let res = str.split('').reverse().join('')
+    return +res
 }
